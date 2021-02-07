f238620394033f0d881028d471f1e275f806ffb0
Alex4386 <psh010209@gmail.com>
2021-02-08 07:18:45 +0900

    chore: bump up version to 1.1.2 :arrow_up:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='XenGarden',
-    version='1.1.1',
+    version='1.1.2',
     description='XenGarden, A Python XenAPI Wrapper for Citrix Hypervisor and XCP-ng Servers',
     author='Stella IT',
     author_email='contact@stella-it.com',
